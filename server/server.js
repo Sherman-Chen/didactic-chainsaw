78871374d0a6b7f09095a77b7ba03a2b8c4f8633
Sherman <shc410@gmail.com>
2017-04-25 16:37:17 -0700

    fix typo in server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const mock = require('./mock.json');
 const logger = require('morgan');
 const chalk = require('chalk');
 const app = express();
 const port = 8080;
 
-const mock = require('../mock.json');
-
+// middleware
 app.use(logger('combined'));
 app.use(cors());
 
+// routes
 app.get('/', (req, res) => {
   res.send('Hello tacocat!');
 });
 
 app.get('/api', (req, res) => {
-  // res.send(JSON.stringify(require('../mock.json')));
   res.json(mock);
 })
 
+// start server
 app.listen(port, () => {
   console.log(chalk.blue(`server started on port ${port}`));
 });
