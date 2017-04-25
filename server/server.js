7e7d7517b2cde44251834e71537e8ea2940365e9
Sherman <shc410@gmail.com>
2017-04-25 15:49:52 -0700

    set CORS for server, bootstrap react-native app

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('morgan');
 const chalk = require('chalk');
 const app = express();
 const port = 8080;
 
+const mock = require('../mock.json');
+
 app.use(logger('combined'));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello tacocat!');
 });
 
 app.get('/api', (req, res) => {
-  res.json(require('../mock.json'));
+  // res.send(JSON.stringify(require('../mock.json')));
+  res.json(mock);
 })
 
 app.listen(port, () => {
