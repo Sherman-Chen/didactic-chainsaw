565ab96f2b8a5a954d7491250f68839bfdb4efc9
Sherman <shc410@gmail.com>
2017-04-26 14:25:07 -0700

    style Success page and add return button to start of demo

--- client/components/Success.js
+++ client/components/Success.js
@@ -4,20 +4,60 @@ import {
   Text,
   View,
   TouchableHighlight,
-  Modal,
-  ScrollView
 } from 'react-native';
 
-const Success = props => {
-  return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  )
+export default class Success extends Component {
+  constructor(props) {
+    super(props);
+    this.returnHome = this.returnHome.bind(this);
+  }
+
+  returnHome() {
+    this.props.navigator.push({
+      id: 'Home'
+    })
+  }
+
+  render() {
+    let { container, text, button, buttonText } = styles;
+    
+    return (
+      <View style={container}>
+        <Text style={text}>Payment Accepted!</Text>
+        <TouchableHighlight
+          onPress={this.returnHome}
+          underlayColor='#0070BA'
+          style={button}
+        >
+          <Text style={buttonText}>Back to Start</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24
+  },
+  button: {
+    marginTop: 10,
+    width: 220,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1072B7',
+    borderRadius: 5
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'white'
+  }
 });
-
-export default Success
