d85089561cdcc991a95a8d7fdd0e80a3fd3a5ceb
Sherman <shc410@gmail.com>
2017-04-25 17:24:33 -0700

    add data fetch flag for app init

--- client/index.ios.js
+++ client/index.ios.js
@@ -21,45 +21,51 @@ export default class client extends Component {
     }
   }
 
-  componentDidMount() {
-    console.log('logging from client2');
-    this.fetchAPI();
-  }
-
-  fetchAPI() {
+  fetchUserData() {
     fetch('http://localhost:8080/api')
       .then(res => {
         return res.json();
       })
       .then(r => {
         this.setState({
-          user: r[0]
+          user: r[0],
+          dataFetched: true
         });
-        console.log(`state is now ${this.state.user.name}`);
       })
       .catch(e => {
         console.error(e);
       })
   }
 
+  componentDidMount() {
+    console.log('logging from client2');
+    this.fetchUserData();
+  }
+
   render() {
-    return (
-      <View>
+    if (this.state.dataFetched) {
+      return (
         <View>
-          <Text>Ship to</Text>
-          <Text>{this.state.user.name}</Text>
+          <View>
+            <Text>Ship to</Text>
+            <Text>{this.state.user.name}</Text>
+          </View>
+          <View>
+            <Text>Pay with</Text>
+          </View>
+          <View>
+            <Text>Total</Text>
+          </View>
+          <Text>View PayPal Policies and your payment method rights.</Text>
+          <Text>Pay Now</Text>
+          <Text>If money is added to your PayPal balance before this transaction completes, the additional balance may be used to complete your payment. Learn More.</Text>
         </View>
-        <View>
-          <Text>Pay with</Text>
-        </View>
-        <View>
-          <Text>Total</Text>
-        </View>
-        <Text>View PayPal Policies and your payment method rights.</Text>
-        <Text>Pay Now</Text>
-        <Text>If money is added to your PayPal balance before this transaction completes, the additional balance may be used to complete your payment. Learn More.</Text>
-      </View>
-    );
+      );
+    } else {
+      return (
+        <Text>App Initializing...</Text>
+      )
+    }
   }
 }
 
